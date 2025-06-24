6d9e4f9f9dedd3328c8724b3c64507d0beb872b9
Chanchal Das <chanchaldas2221@gmail.com>
2025-06-24 16:19:37 +0530

    commit

--- frontend/src/utilis/axios.js
+++ frontend/src/utilis/axios.js
@@ -3,9 +3,6 @@ import axios from 'axios';
 export default axios.create({
   baseURL: 'http://localhost:4000',
   withCredentials: true,
-  headers: {
-    'Content-Type': 'application/json',
-  },
 });
 
 // import axios from 'axios';
